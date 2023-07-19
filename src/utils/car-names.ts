37b8a65a64e250cbb97fd40153edb013359c0e1c
Sergey Vergun <sewergun@gmail.com>
2023-07-19 15:54:41 +0300

    feat: add car names and models list

---
+++ src/utils/car-names.ts
@@ -0,0 +1,25 @@
+export const carBrands = [
+  "CLAAS",
+  "Terrion",
+  "MTZ",
+  "Caterpillar",
+  "Steyr",
+  "John Deere",
+  "Komatsu",
+  "Lamborghini",
+  "Mahindra",
+  "TYM",
+];
+
+export const carModels = [
+  "ARION",
+  "ATM 7360",
+  "Belarus",
+  "420",
+  "Terrus CVT",
+  "6195M",
+  "WB97S-8",
+  "Spark R",
+  "NOVO 755 DI",
+  "T1104",
+];
